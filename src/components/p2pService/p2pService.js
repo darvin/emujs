9eb02ffbba04cac1a63c717aee26ab1fccb24032
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-05-27 04:40:10 -0700

    group peers

--- src/components/p2pService/p2pService.js
+++ src/components/p2pService/p2pService.js
@@ -1,44 +1,81 @@
 angular.module( 'p2pService', [])
 
 .factory( 'p2pService', function (  ) {
-  
+  var COMMAND_PREFIX = "_com",
+      COMMAND_KNOWN_PEERS = "known";
   var p2pService = {
     createClient:function(clientId, opts) {
       var peer = new Peer(clientId, 
         {
         key: 'qbxxtma1ws7qr529',
         host:"0.peerjs.com",
-        port:"9000",
-        debug:true
+        port:"9000"
       }
         /*{host: 'localhost', port: 9000}*/
       );
 
+      var lazySend = function(conn, data) {
+        if (conn.open) {
+          conn.send(data);
+        } else {
+          var sendOnOpen = function() {
+            conn.send(data);
+            conn.off("open", sendOnOpen);
+          };
+          conn.on("open", sendOnOpen);
+        }
+      };
 
       var connections = {};
+      var getKnownPeersForConn = function(conn) {
+        return Object.keys(connections).filter(function(peerId){return conn.peer!=peerId;});
+      };
+
+      var connect = function(peerId) {
+        var conn = peer.connect(peerId);
+        conn.on('data', onReceived);
+        connections[peerId] = conn;
+      };  
+      var connectToUnknownPeers = function(knownPeers) {
+        angular.forEach(knownPeers, function(peerId){
+          if (!connections[peerId]) {
+            console.error("connecting "+peer.id+ " to "+peerId);
+            connect(peerId);
+          }
+        });
+      };
+      var processCommand = function(command, data) {
+        switch(command){
+          case COMMAND_KNOWN_PEERS:
+            if (data.length>0) {
+              connectToUnknownPeers(data);
+            }
+            break;
+        }
+      };
+      var onReceived = function(data) {
+        if (data instanceof Array && data[0]==COMMAND_PREFIX) {
+          processCommand(data[1], data[2]);
+        } else{
+          opts.onReceived(data);
+
+        }
+      };
 
       peer.on('connection', function(conn) {
-        console.error("HI");
-
         connections[conn.peer] = conn;
-        conn.on('data', opts.onReceived);
+        conn.on('data', onReceived);
+
         opts.onConnected(conn.peer);
+        lazySend(conn, [COMMAND_PREFIX, COMMAND_KNOWN_PEERS, getKnownPeersForConn(conn)]);
 
       });
 
       return {
-        connect: function(connectToClientId) {
-          console.error("connecting "+peer.id + "to "+connectToClientId);
-
-          var conn = peer.connect(connectToClientId);
-          conn.on('data', opts.onReceived);
-          connections[connectToClientId] = conn;
-        },
+        connect: connect,
         send: function(data) {
-          console.error(data, connections);
           angular.forEach(connections, function(conn, peerId){
-            console.error("seding from "+peer.id+" to "+conn.peer);
-            conn.send(data);
+            lazySend(conn, data);
           });
         }
       };
--- src/components/p2pService/p2pService.spec.js
+++ src/components/p2pService/p2pService.spec.js
@@ -41,5 +41,59 @@ describe( 'p2pService', function() {
       expect(onReceived2.callCount).toBe(1);
     });
   }));
+
+  it( 'should talk to each other in groups', inject( function() {
+    var onReceived1 = jasmine.createSpy(),
+        onReceived2 = jasmine.createSpy(),
+        onReceived3 = jasmine.createSpy(),
+        onConnected1 = jasmine.createSpy(),
+        onConnected2 = jasmine.createSpy(),
+        onConnected3 = jasmine.createSpy();
+    var peer1, peer2;
+
+
+    runs(function(){
+      peer1 = p2pService.createClient("groupPeer1", {
+        onReceived:onReceived1,
+        onConnected:onConnected1
+      }),
+      peer2 = p2pService.createClient("groupPeer2", {
+        onReceived:onReceived2,
+        onConnected:onConnected2});
+      peer3 = p2pService.createClient("groupPeer3", {
+        onReceived:onReceived3,
+        onConnected:onConnected3});
+
+      peer2.connect("groupPeer1");
+      peer3.connect("groupPeer1");
+    });
+    waits(1000);
+    runs(function(){
+
+      expect(onConnected1).toHaveBeenCalledWith("groupPeer2");
+      expect(onConnected2).toHaveBeenCalledWith("groupPeer3");
+      peer1.send({some:"dataFromPeer1"});
+    });
+    waits(300);
+    runs(function(){  
+      expect(onReceived2).toHaveBeenCalledWith({some:"dataFromPeer1"});
+      expect(onReceived3).toHaveBeenCalledWith({some:"dataFromPeer1"});
+      expect(onReceived2.callCount).toBe(1);
+      expect(onReceived3.callCount).toBe(1);
+      expect(onReceived1.callCount).toBe(0);
+    });
+
+    runs(function(){
+      peer2.send(["dataFromPeer2"]);
+    });
+    waits(300);
+    runs(function(){  
+      expect(onReceived1).toHaveBeenCalledWith(["dataFromPeer2"]);
+      expect(onReceived1.callCount).toBe(1);
+
+    });
+
+  }));
+
 });
 
